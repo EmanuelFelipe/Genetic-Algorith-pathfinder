1e390adfb0bf86237eed8892ba1184e8a6fda8c1
Emanuel Felipe <EmanuelFelipe0501@gmail.com>
2020-05-07 19:35:23 -0300

    proximas gerações crossover

--- AlgoritmoGenetico/AlgoritmoGenetico.c
+++ AlgoritmoGenetico/AlgoritmoGenetico.c
@@ -6,29 +6,30 @@
 #define PAREDE 1
 #define SAIDA 4
 #define inicio 2
+#define TETO 3
 
-int tamGenes = 2;
-int tamPop = 50;
 int numGeracoes = 100;
 float probMut = 0.1;
 float probCruz = 0.7;
 int parede = 0;
 int saida = 0;
+int geracaoAtual = 0;
+
+int i = 0, j = 0, k = 0, l = 0;
+int vet[4];
+
+int individ1[80][100],
+    individ2[80][100],
+    individ3[80][100],
+    individ4[80][100];
 
 int coluna_atual = 1,
     linha_atual = 1,
-    coluna_atual2 = 1,
-    linha_atual2 = 1;
+    coluna_anterior = 1,
+    linha_anterior = 1;
 
 int key,
     passos;
-
-typedef struct individuo
-{
-    int x;
-    int y;
-    int fitness;
-} individuo;
 
 int mapa[20][50];
 
@@ -36,6 +37,7 @@ void percurso(int x);
 void gera_mapa();
 int desenhar_mapa();
 void iniciaPopulacao();
+void crossOver();
 void paraBaixo();
 void paraCima();
 void paraDireita();
@@ -54,11 +56,13 @@ void gera_mapa()
             {
                 mapa[m][n] = rand() % 3;
 
+                mapa[1][1] = 0;
                 if (mapa[m][n] == 2)
                 {
                     mapa[m][n] = 0;
                 }
             }
+            mapa[0][n] = 3;
         }
     }
     n = rand() % 50;
@@ -85,6 +89,8 @@ int desenhar_mapa()
                 printf("|");
             if (mapa[linha][coluna] == SAIDA)
                 printf("#");
+            if (mapa[linha][coluna] == TETO)
+                printf("-");
         }
         printf("\n");
     }
@@ -93,14 +99,13 @@ int desenhar_mapa()
 void iniciaPopulacao()
 {
     srand(time(NULL));
-    int individ[80];
-    int i = 0;
     do
     {
-        individ[i] = rand() % 3;
-        percurso(individ[i]);
+        individ1[i][geracaoAtual] = rand() % 3;
+        printf("\nindividuo 1\n");
+        printf("crom: %d", individ1[i][geracaoAtual]);
+        percurso(individ1[i][geracaoAtual]);
         i++;
-
         if (saida == 1)
         {
             break;
@@ -110,31 +115,94 @@ void iniciaPopulacao()
             break;
         }
     } while (i < 80);
+    coluna_atual = 1;
+    linha_atual = 1;
+    parede = 0;
+    saida = 0;
+    do
+    {
+        individ2[j][geracaoAtual] = rand() % 3;
+        printf("\nindividuo 2\n");
+        printf("crom: %d", individ2[j][geracaoAtual]);
+        percurso(individ2[j][geracaoAtual]);
+        j++;
+
+        if (saida == 1)
+        {
+            break;
+        }
+        else if (parede == 1)
+        {
+            break;
+        }
+    } while (j < 80);
+    coluna_atual = 1;
+    linha_atual = 1;
+    parede = 0;
+    saida = 0;
+    do
+    {
+        individ3[k][geracaoAtual] = rand() % 3;
+        printf("\nindividuo 3\n");
+        printf("crom: %d", individ3[k][geracaoAtual]);
+        percurso(individ3[k][geracaoAtual]);
+        k++;
+
+        if (saida == 1)
+        {
+            break;
+        }
+        else if (parede == 1)
+        {
+            break;
+        }
+    } while (k < 80);
+    coluna_atual = 1;
+    linha_atual = 1;
+    parede = 0;
+    saida = 0;
+    do
+    {
+        individ4[l][geracaoAtual] = rand() % 3;
+        printf("\nindividuo 4\n");
+        printf("crom: %d", individ4[l][geracaoAtual]);
+        percurso(individ4[l][geracaoAtual]);
+        l++;
+
+        if (saida == 1)
+        {
+            break;
+        }
+        else if (parede == 1)
+        {
+            break;
+        }
+    } while (l < 80);
+    //desenhar_mapa();
 }
 
-void percurso(int x)
+void percurso(int opcao)
 {
-    int opcao = x;
     switch (opcao)
     {
     case 0:
         paraBaixo();
-        system("clear");
+        printf("\n");
         desenhar_mapa();
         break;
     case 1:
         paraCima();
-        system("clear");
+        printf("\n");
         desenhar_mapa();
         break;
     case 2:
         paraDireita();
-        system("clear");
+        printf("\n");
         desenhar_mapa();
         break;
     case 3:
         paraEsquerda();
-        system("clear");
+        printf("\n");
         desenhar_mapa();
         break;
 
@@ -144,12 +212,160 @@ void percurso(int x)
     }
 }
 
+void crossOver()
+{
+    srand(time(NULL));
+    int x, escolha, aux;
+    geracaoAtual++;
+    parede = 0;
+    saida = 0;
+    coluna_atual = 1;
+    linha_atual = 1;
+    int a = 0;
+    do
+    {
+        if (a == 0)
+        {
+            escolha = rand() % 3;
+            aux = escolha;
+            if (escolha == 0)
+            {
+                x = rand() % i;
+                individ1[a][geracaoAtual] = individ1[x][geracaoAtual - 1];
+            }
+            else if (escolha == 1)
+            {
+                x = rand() % j;
+                individ1[a][geracaoAtual] = individ2[x][geracaoAtual - 1];
+            }
+            else if (escolha == 2)
+            {
+                x = rand() % k;
+                individ1[a][geracaoAtual] = individ3[x][geracaoAtual - 1];
+            }
+            else if (escolha == 3)
+            {
+                x = rand() % l;
+                individ1[a][geracaoAtual] = individ4[x][geracaoAtual - 1];
+            }
+        }
+        else
+        {
+            if (escolha == aux)
+            {
+                escolha = rand() % 3;
+            }
+            if (escolha == 0)
+            {
+                individ1[a][geracaoAtual] = individ1[a][geracaoAtual - 1];
+            }
+            else if (escolha == 1)
+            {
+                individ1[a][geracaoAtual] = individ2[a][geracaoAtual - 1];
+            }
+            else if (escolha == 2)
+            {
+                individ1[a][geracaoAtual] = individ3[a][geracaoAtual - 1];
+            }
+            else if (escolha == 3)
+            {
+                individ1[a][geracaoAtual] = individ4[a][geracaoAtual - 1];
+            }
+        }
+        printf("\nindividuo 1 ger 2\n");
+        printf("escolha %d\n", escolha);
+        printf("x %d\n", x);
+        printf("a %d\n", a);
+        percurso(individ1[a][geracaoAtual]);
+        if (saida == 1)
+        {
+            break;
+        }
+        else if (parede == 1)
+        {
+            break;
+        }
+        a++;
+    } while (a < 80);
+    parede = 0;
+    saida = 0;
+    coluna_atual = 1;
+    linha_atual = 1;
+    for (int b = 0; b < i; b++)
+    {
+        if (b == 0)
+        {
+            x = rand() % i;
+            escolha = rand() % 3;
+            aux = escolha;
+            if (escolha == 0)
+            {
+                x = rand() % i;
+                individ2[b][geracaoAtual] = individ1[x][geracaoAtual - 1];
+            }
+            else if (escolha == 1)
+            {
+                x = rand() % j;
+                individ2[b][geracaoAtual] = individ2[x][geracaoAtual - 1];
+            }
+            else if (escolha == 2)
+            {
+                x = rand() % k;
+                individ2[b][geracaoAtual] = individ3[x][geracaoAtual - 1];
+            }
+            else if (escolha == 3)
+            {
+                x = rand() % l;
+                individ2[b][geracaoAtual] = individ4[x][geracaoAtual - 1];
+            }
+        }
+        else
+        {
+            do
+            {
+                escolha = rand() % 3;
+            } while (escolha == aux);
+            if (escolha == 0)
+            {
+                individ2[b][geracaoAtual] = individ1[b][geracaoAtual - 1];
+            }
+            else if (escolha == 1)
+            {
+                individ2[b][geracaoAtual] = individ2[b][geracaoAtual - 1];
+            }
+            else if (escolha == 2)
+            {
+                individ2[b][geracaoAtual] = individ3[b][geracaoAtual - 1];
+            }
+            else if (escolha == 3)
+            {
+                individ2[b][geracaoAtual] = individ4[b][geracaoAtual - 1];
+            }
+        }
+        printf("\nindividuo 2 ger 2\n");
+        printf("escolha %d\n", escolha);
+        printf("x %d\n", x);
+        printf("b %d\n", b);
+        percurso(individ1[b][geracaoAtual]);
+        if (saida == 1)
+        {
+            break;
+        }
+        else if (parede == 1)
+        {
+            break;
+        }
+    }
+    printf("teste: %d", individ1[0][geracaoAtual]);
+    int mostra = 0;
+}
+
 void paraBaixo()
 {
     linha_atual = 1 + linha_atual;
     passos = passos + 1;
 
-    if ((mapa[linha_atual][coluna_atual] == PAREDE))
+    if (mapa[linha_atual][coluna_atual] == PAREDE || mapa[linha_atual][coluna_atual] == TETO)
     {
         parede = 1;
         linha_atual -= 1;
@@ -165,7 +381,7 @@ void paraCima()
     linha_atual = linha_atual - 1;
     passos = passos + 1;
 
-    if (mapa[linha_atual][coluna_atual] == PAREDE)
+    if (mapa[linha_atual][coluna_atual] == PAREDE || mapa[linha_atual][coluna_atual] == TETO)
     {
         parede = 1;
         linha_atual = linha_atual + 1;
@@ -181,7 +397,7 @@ void paraDireita()
     coluna_atual = coluna_atual + 1;
     passos = passos + 1;
 
-    if (mapa[linha_atual][coluna_atual] == PAREDE)
+    if (mapa[linha_atual][coluna_atual] == PAREDE || mapa[linha_atual][coluna_atual] == TETO)
     {
         parede = 1;
         coluna_atual = coluna_atual - 1;
@@ -197,7 +413,7 @@ void paraEsquerda()
     coluna_atual = coluna_atual - 1;
     passos = passos + 1;
 
-    if (mapa[linha_atual][coluna_atual] == PAREDE)
+    if (mapa[linha_atual][coluna_atual] == PAREDE || mapa[linha_atual][coluna_atual] == TETO)
     {
         parede = 1;
         coluna_atual = coluna_atual + 1;
@@ -210,21 +426,32 @@ void paraEsquerda()
 
 int main()
 {
-
+    int pergunta, perg;
     gera_mapa();
     desenhar_mapa();
-    int pergunta, perg;
 
-    printf("\ncomecar labirinto?");
+    printf("começar labirinto?1 - sim, 2 - nao\n");
     scanf("%d", &pergunta);
+
     if (pergunta == 1)
     {
         iniciaPopulacao();
     }
     else
     {
-        printf("ta bom, tchau\n");
-        system("exit");
+        printf("ta bom, tchau");
+    }
+
+    printf("começar proxima geracao?1 - sim, 2 - nao\n");
+    scanf("%d", &pergunta);
+
+    if (pergunta == 1)
+    {
+        crossOver();
+    }
+    else
+    {
+        printf("ta bom, tchau");
     }
 
     return 0;
