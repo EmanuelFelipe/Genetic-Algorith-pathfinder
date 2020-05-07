fb09a91f7c65503d7e6123e35af237d9fa26369a
Emanuel Felipe <EmanuelFelipe0501@gmail.com>
2020-05-06 21:07:28 -0300

    primeiro individuo(amem papai do ceu)

--- AlgoritmoGenetico/AlgoritmoGenetico.c
+++ AlgoritmoGenetico/AlgoritmoGenetico.c
@@ -7,35 +7,58 @@
 #define SAIDA 4
 #define inicio 2
 
-    int tamGenes = 2;
-    int tamPop = 50;
-    int numGeracoes = 100;
-    float probMut = 0.1;
-    float probCruz = 0.7;
+int tamGenes = 2;
+int tamPop = 50;
+int numGeracoes = 100;
+float probMut = 0.1;
+float probCruz = 0.7;
+int parede = 0;
+int saida = 0;
 
-    int coluna_atual = 1,
+int coluna_atual = 1,
     linha_atual = 1,
     coluna_atual2 = 1,
     linha_atual2 = 1;
 
-    int key,
+int key,
     passos;
 
-typedef struct individuo{
+typedef struct individuo
+{
     int x;
     int y;
     int fitness;
-}individuo;
+} individuo;
 
-int mapa[20][20];
+int mapa[10][20] = {
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,1,
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,};;
 
-void gera_mapa() {
+void percurso(int x);
+void gera_mapa();
+int desenhar_mapa();
+void iniciaPopulacao();
+void paraBaixo();
+void paraCima();
+void paraDireita();
+void paraEsquerda();
+
+void gera_mapa()
+{
     int m, n;
     srand(time(NULL));
 
-    for (m = 0; m < 20;m++)
+    for (m = 0; m < 10; m++)
     {
-        for (n = 0;n<20;n++)
+        for (n = 0; n < 20; n++)
         {
             if (mapa[m][n] == 0)
             {
@@ -49,77 +72,158 @@ void gera_mapa() {
         }
     }
     n = rand() % 20;
-    m = rand() % 20;
+    m = rand() % 10;
     mapa[m][n] = 4;
 }
 
-int desenhar_mapa(int mapa[20][20]) {
-    int linha,coluna;
-	int coluna_atual = 1, linha_atual = 1, coluna_atual2 = 1, linha_atual2 = 1;
+int desenhar_mapa()
+{
+    int linha, coluna;
+    for (linha = 0; linha < 10; linha++)
+    {
+        for (coluna = 0; coluna < 20; coluna++)
+        {
 
-    for (linha=0;linha<20;linha++) {
-        for (coluna=0;coluna<20;coluna++) {
-
-            if ((linha == linha_atual) && (coluna == coluna_atual)) {
-                printf("*");
+            if ((linha == linha_atual) && (coluna == coluna_atual))
+            {
+                printf(".");
                 continue;
             }
             if (mapa[linha][coluna] == CHAO)
                 printf(" ");
             if (mapa[linha][coluna] == PAREDE)
-                printf("-");
+                printf("|");
             if (mapa[linha][coluna] == SAIDA)
                 printf("#");
-
         }
         printf("\n");
     }
     return 1;
 }
-void iniciaPopulacao(){
+void iniciaPopulacao()
+{
+    srand(time(NULL));
+    int individ[80];
+        int i = 0;
+        do{
+        individ[i] = rand() % 3;
+        percurso(individ[i]);
+        i++;
+
+        if (saida == 1){
+            break;
+        }//else if(parede == 1){
+        //    break;
+        //}
+        
+        }while(i<80);
+        
     
 }
 
-int paraBaixo(){
-    linha_atual = 1 + linha_atual;
-    passos = passos + 1;
+void percurso(int x)
+{
+    int opcao = x;
+    switch (opcao)
+    {
+    case 0:
+        paraBaixo();
+        system("clear");
+        desenhar_mapa();
+        break;
+    case 1:
+        paraCima();
+        system("clear");
+        desenhar_mapa();
+        break;
+    case 2:
+        paraDireita();
+        system("clear");
+        desenhar_mapa();
+        break;
+    case 3:
+        paraEsquerda();
+        system("clear");
+        desenhar_mapa();
+        break;
 
-    if(mapa[linha_atual][coluna_atual] == PAREDE){
-        linha_atual = linha_atual - 1;
+    default:
+        printf("\nopção invalida");
+        break;
     }
 }
 
-int paraCima(){
+void paraBaixo()
+{
+    linha_atual = 1 + linha_atual;
+    passos = passos + 1;
+    
+    if ((mapa[linha_atual][coluna_atual] == PAREDE)){
+        parede = 1;
+        linha_atual -= 1;
+    }else if(mapa[linha_atual][coluna_atual] == SAIDA){
+        saida = 1;
+    }
+}
+
+void paraCima()
+{
     linha_atual = linha_atual - 1;
     passos = passos + 1;
 
     if(mapa[linha_atual][coluna_atual] == PAREDE){
+        parede = 1;
         linha_atual = linha_atual + 1;
+    }else if(mapa[linha_atual][coluna_atual] == SAIDA){
+        saida = 1;
     }
 }
 
-int paraDireita(){
+void paraDireita()
+{
     coluna_atual = coluna_atual + 1;
     passos = passos + 1;
 
     if(mapa[linha_atual][coluna_atual] == PAREDE){
+        parede = 1;
         coluna_atual = coluna_atual - 1;
+    }else if(mapa[linha_atual][coluna_atual] == SAIDA){
+        saida = 1;
     }
 }
 
-int paraEsquerda(){
+void paraEsquerda()
+{
     coluna_atual = coluna_atual - 1;
     passos = passos + 1;
 
     if(mapa[linha_atual][coluna_atual] == PAREDE){
+        parede = 1;
         coluna_atual = coluna_atual + 1;
+    }else if(mapa[linha_atual][coluna_atual] == SAIDA){
+        saida = 1;
     }
 }
 
-int main(){
+int main()
+{
 
-	gera_mapa();
-    desenhar_mapa(mapa);
+    gera_mapa();
+    desenhar_mapa();
+    int pergunta, perg;
+
+
+    printf("\ncomecar labirinto?");
+    scanf("%d", &pergunta);
+    if (pergunta == 1)
+    {
+        iniciaPopulacao();
+    }
+    else
+    {
+        printf("ta bom, tchau\n");
+        system("exit");
+    }
 
     return 0;
 }
